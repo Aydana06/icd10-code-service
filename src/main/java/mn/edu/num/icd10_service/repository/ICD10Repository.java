a44e2318dd26a48e17c18a387a78a2fe1a43ac97
Bolat Aydana <bolataidana73@gmail.com>
2026-09-17 09:31:32 +0800

    Add caching to ICD10 service

--- src/main/java/mn/edu/num/icd10_service/repository/ICD10Repository.java
+++ src/main/java/mn/edu/num/icd10_service/repository/ICD10Repository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Repository;
 
+import jakarta.annotation.PostConstruct;
 import mn.edu.num.icd10_service.domain.Chapter;
 import mn.edu.num.icd10_service.domain.ICD10Wrapper;
 import tools.jackson.databind.ObjectMapper;
@@ -13,18 +14,22 @@ import tools.jackson.databind.ObjectMapper;
 @Repository
 public class ICD10Repository {
 
-	public List<Chapter> findAll() {
-		try {
-			ObjectMapper mapper = new ObjectMapper();
+	private List<Chapter> chapters;
 
-			InputStream is = new ClassPathResource("icd10_code.json").getInputStream();
-
-			ICD10Wrapper wrapper = mapper.readValue(is, ICD10Wrapper.class);
-
-			return wrapper.getChapters();
-
+	@PostConstruct 
+	public void loadData(){
+		try{
+			ObjectMapper mapper = new ObjectMapper(); 
+			try(InputStream is = new ClassPathResource("icd10_code.json").getInputStream()){
+				ICD10Wrapper wrapper = mapper.readValue(is, ICD10Wrapper.class);
+				this.chapters = wrapper.getChapters();
+			}
 		} catch (Exception e) {
-			throw new RuntimeException("Failed to read ICD10 JSON", e);
+			throw new RuntimeException("Failed to read ICD10 json", e);	
 		}
+	}
+
+	public List<Chapter> findAll() {
+		return chapters;
 	}
 }
